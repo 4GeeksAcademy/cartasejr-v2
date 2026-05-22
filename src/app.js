83e931138f77c011844a938de07b38f8226afd31
Ejrodriguez3636 <ejrodriguez3636@gmail.com>
2026-05-22 10:46:56 +0000

    casi casi terminado cartas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,15 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let cardSimbolos = ['♣', '♦', '♥', '♠'];
+  let cardNumeros = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+  let paloRandom = cardSimbolos[Math.floor(Math.random()*cardSimbolos.length)]
+  let numeroRandom = cardNumeros[Math.floor(Math.random()*cardNumeros.length)]
+  console.log(paloRandom);
+  console.log(numeroRandom)
+  let paloTop = document.getElementById("paloTop")
+  paloTop.innerHTML=paloRandom
+
+
 };
