47a0d71ef198eedfe8279e5d8a4ed04ba6558181
Ejrodriguez3636 <ejrodriguez3636@gmail.com>
2026-05-24 20:02:19 +0000

    carta aleatoria termaninado

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import "bootstrap";
 import "./style.css";
 
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
@@ -10,12 +9,20 @@ window.onload = function() {
   let cardSimbolos = ['♣', '♦', '♥', '♠'];
   let cardNumeros = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
-  let paloRandom = cardSimbolos[Math.floor(Math.random()*cardSimbolos.length)]
-  let numeroRandom = cardNumeros[Math.floor(Math.random()*cardNumeros.length)]
-  console.log(paloRandom);
-  console.log(numeroRandom)
-  let paloTop = document.getElementById("paloTop")
-  paloTop.innerHTML=paloRandom
+  let paloRandom = cardSimbolos[Math.floor(Math.random() * cardSimbolos.length)];
+  let numeroRandom = cardNumeros[Math.floor(Math.random() * cardNumeros.length)];
 
+  let paloTop = document.getElementById("paloTop");
+  let paloBottom = document.getElementById("paloBottom");
+  let numero = document.getElementById("numero");
 
-};
+  paloTop.innerHTML = paloRandom;
+  paloBottom.innerHTML = paloRandom;
+  numero.innerHTML = numeroRandom;
+if (paloRandom === '♥' || paloRandom === '♦') {
+  paloTop.style.color = "red";
+  paloBottom.style.color = "red";
+} else {
+  paloTop.style.color = "black";
+  paloBottom.style.color = "black";
+}};
